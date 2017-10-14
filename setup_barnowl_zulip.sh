59eaf3a3fb57c558324c42dde80fd9c6ea4b9830
Christian Ternus <ternus@cternus.net>
2017-10-14 14:47:18 -0400

    Add new dependencies to avoid startup errors.

--- setup_barnowl_zulip.sh
+++ setup_barnowl_zulip.sh
@@ -11,18 +11,17 @@ apt-get install -y autoconf gcc pkgconf libncursesw5-dev \
    libnet-dns-perl libauthen-sasl-perl libio-socket-ssl-perl libdigest-perl \
    libanyevent-irc-perl libclass-accessor-perl libtext-autoformat-perl \
    libextutils-depends-perl libmodule-install-perl libanyevent-http-perl \
-   liburi-encode-perl libglib-perl
-#apt-get install -y barnowl
+   liburi-encode-perl libglib-perl libnet-twitter-lite-perl libany-moose-perl \
+   libmime-base64-urlsafe-perl libouch-perl libdatetime-perl \
+   libdatetime-format-strptime-perl
 
 git clone https://github.com/ternus/barnowl
 pushd barnowl
 git checkout zulip
 ./autogen.sh
 ./configure --without-zephyr
-make -j8 install
-
-# Remove unneeded modules
-rm -f /usr/local/share/barnowl/modules/{Facebook,Twitter}.par
+make -j8 
+make install
 popd
 
 sed "s|ZULIP_USERNAME|$ZULIP_USERNAME|" -i .owl/zulip
